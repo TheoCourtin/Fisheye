3e49b6cae8ab82b47ff235d2aef26dfb1566f610
Théo COURTIN <34097019+TheoCourtin@users.noreply.github.com>
2022-10-13 10:29:09 +0200

    Add files via upload

---
+++ scripts/pages/index.js
@@ -0,0 +1,26 @@
+async function getPhotographers() {
+  // Penser à remplacer par les données récupérées dans le json
+
+  return fetch("data/photographers.json")
+    .then((res) => res.json())
+    .then((json) => json);
+}
+
+async function displayData(photographers) {
+  const photographersSection = document.querySelector(".photographer_section");
+
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerFactory(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  console.log(photographers);
+  displayData(photographers);
+}
+
+init();
