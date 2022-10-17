23e4e6f49c19f25fd148494f5e2982c3a3a19ae3
Théo COURTIN <34097019+TheoCourtin@users.noreply.github.com>
2022-10-17 07:34:16 +0200

    Update photographer.js

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,27 @@
 function photographerFactory(data) {
   const { name, id, portrait, city, country, tagline, price } = data;
 
-  const picture = `assets/photographers/${portrait}`;
-
+  const picture = `assets/photographers/photographers_id_photos/${portrait}`;
+  
   function getUserCardDOM() {
     const article = document.createElement("article");
+
+    //Id photographe
+    article.id = "photographer" + id;
+
+    //Lien a 
+    const url = document.createElement("a");
+
     const img = document.createElement("img");
     img.setAttribute("src", picture);
-    img.setAttribute("alt", "Photo de profil de ${name}");
+    img.setAttribute("alt", `Photo de profil de ${name}`);
+    img.setAttribute("class", "link-profil");
+    
+
     const h2 = document.createElement("h2");
-    h2.textContent = name;
-    article.appendChild(img);
-    article.appendChild(h2);
+    h2.setAttribute("class", "link-profil");
+    h2.textContent = name;   
+    
 
     //const id = document.createElement( 'id' );
     //article.appendChild(id);
@@ -19,22 +29,33 @@ function photographerFactory(data) {
     //const country = document.createElement( 'country' );
     const spanCountry = document.createElement("span");
     spanCountry.textContent = city.concat(",", " ", country);
-    article.appendChild(spanCountry);
+    
 
     //const tagline = document.createElement( 'tagline' );
     const pTagline = document.createElement("p");
     pTagline.textContent = tagline;
-    article.appendChild(pTagline);
+    
 
     //const price = document.createElement( 'price' );
     const divPrice = document.createElement("div");
     divPrice.setAttribute("class", "price");
     divPrice.textContent = price + "€/jour";
-    article.appendChild(divPrice);
+    
 
     //console.log(id,city,country,tagline,price);
+
+    url.href="photographer.html?id=" + id;
+    article.appendChild(url);
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(spanCountry);
+    article.appendChild(pTagline);
+    article.appendChild(divPrice);
+
     return article;
   }
-  return { name, id, picture, city, country, tagline, price, getUserCardDOM };
-  // return {  id, getUserCardDOM }
+  return { getUserCardDOM };
+  //return { name, id, picture, city, country, tagline, price, getUserCardDOM };
+  
+  
 }
