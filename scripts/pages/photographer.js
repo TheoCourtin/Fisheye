243edcb48a6c31d1aa16f0ad783259eb464f5cf0
Théo COURTIN <34097019+TheoCourtin@users.noreply.github.com>
2022-10-17 16:27:16 +0200

    Update photographer.js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,18 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+// Récupération de l'ID dans l'url
+function getPhotographerId() {
+  return new URL(location.href).searchParams.get("id");
+}
+const photographerId = getPhotographerId();
+
+console.log(photographerId);
+
+// async function displayProfile(photographers) {
+//   photographers.forEach((photographer) => {
+//     if (photographer.id == photographerId) {
+//       const photographersData = photographerFactory(photographer);
+//       photographersData.getPhotographerProfilDOM();
+//     }
+//   });
+// }
